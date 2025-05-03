b81a5d22b5bb6b18be7ade8dd8c0813e275aa3d8
johncazier <johncazier@gmail.com>
2025-05-03 15:21:03 -0600

    minor tweaks to make it work how I want

--- app/src/main/java/com/boardgamegeek/ui/viewmodel/CollectionDetailsViewModel.kt
+++ app/src/main/java/com/boardgamegeek/ui/viewmodel/CollectionDetailsViewModel.kt
@@ -24,6 +24,8 @@ import com.boardgamegeek.repository.PlayRepository
 import com.boardgamegeek.repository.StatsHelper.Companion.calculateCorrelationCoefficient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneId
 import javax.inject.Inject
 
 @HiltViewModel
@@ -343,7 +345,7 @@ class CollectionDetailsViewModel @Inject constructor(
         liveData {
             val list = it.asSequence()
                 .filterBaseGames()
-                .filter { it.lastPlayDate != null && it.lastPlayDate > 0L }
+                .filter { it.lastPlayDate != null && it.lastPlayDate > LocalDateTime.now().atZone(ZoneId.systemDefault()).minusDays(30).toInstant().toEpochMilli() }
                 .sortedByDescending { it.lastPlayDate }
                 .toList()
             emit(
@@ -357,7 +359,7 @@ class CollectionDetailsViewModel @Inject constructor(
             val list = it.asSequence()
                 .filterOwned()
                 .filterBaseGames()
-                .filter { it.friendlessShouldPlay > 10_000.0 }
+                .filter { it.rating >= 8 && it.friendlessShouldPlay > 1270 }
                 .toList()
             emit(
                 list.sortedByDescending { it.friendlessShouldPlay }.take(ITEM_LIMIT) to list.size
@@ -597,7 +599,7 @@ class CollectionDetailsViewModel @Inject constructor(
     }
 
     companion object {
-        const val ITEM_LIMIT = 5
+        const val ITEM_LIMIT = 30
         const val UNPUBLISHED_PROTOTYPE_ID = 18291
         const val WORK_NAME = "CollectionViewModel"
     }
