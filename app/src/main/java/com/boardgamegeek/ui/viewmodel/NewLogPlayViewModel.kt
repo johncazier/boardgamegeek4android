c481415b10c18020af8ec60a93402fefe6994773
johncazier <johncazier@gmail.com>
2025-05-03 16:15:53 -0600

    add support for Compose and start new log play activity that uses Compose

---
+++ app/src/main/java/com/boardgamegeek/ui/viewmodel/NewLogPlayViewModel.kt
@@ -0,0 +1,12 @@
+package com.boardgamegeek.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NewLogPlayViewModel @Inject constructor(
+) : ViewModel() {
+
+
+}
