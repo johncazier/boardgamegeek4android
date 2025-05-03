140d08c0569c33e8795453cf2499e53c1a79ebb1
johncazier <johncazier@gmail.com>
2025-05-03 17:03:39 -0600

    add Coil to show images from Compose

--- app/src/main/java/com/boardgamegeek/ui/NewLogPlayScreen.kt
+++ app/src/main/java/com/boardgamegeek/ui/NewLogPlayScreen.kt
@@ -2,12 +2,19 @@ package com.boardgamegeek.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.boardgamegeek.ui.viewmodel.NewLogPlayViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,12 +23,34 @@ fun NewLogPlayScreen(viewModel: NewLogPlayViewModel) {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Log Play") })
+            TopAppBar(
+                title = { Text(text = "Log Play") },
+                navigationIcon = {
+                    IconButton(onClick = { viewModel.cancel() }) {
+                        Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { viewModel.save() }) {
+                        Icon(imageVector = Icons.Filled.Done, contentDescription = "Save")
+                    }
+                }
+            )
         }
     ) { padding ->
 
-        Column(modifier = Modifier.padding(padding)) {
-            Text("Game ID: ${viewModel.gameId}, name: ${viewModel.gameName}")
+        Column(modifier = Modifier
+            .padding(padding)
+        ) {
+            AsyncImage(
+                model = viewModel.gameImageUrl,
+                contentDescription = null
+            )
+
+            Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+
+                Text("Game image url: ${viewModel.gameImageUrl}")
+            }
         }
     }
 }
