193a3462782c576941888921e28091e9ee344194
johncazier <johncazier@gmail.com>
2025-05-10 18:57:15 -0600

    create date field component

--- app/src/main/java/com/boardgamegeek/ui/viewmodel/NewLogPlayViewModel.kt
+++ app/src/main/java/com/boardgamegeek/ui/viewmodel/NewLogPlayViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.boardgamegeek.ui.NewLogPlayRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
 class NewLogPlayViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     val route = savedStateHandle.get<NewLogPlayRoute>("route")!!
@@ -18,6 +20,12 @@ class NewLogPlayViewModel @Inject constructor(
     val gameName = route.gameName
 
     val gameImageUrl = route.gameImageUrl
+
+    val selectedDateFlow: StateFlow<LocalDate> = savedStateHandle.getStateFlow("selectedDate", LocalDate.now())
+
+    fun updateSelectedDate(selectedDate: LocalDate?) {
+        savedStateHandle["selectedDate"] = selectedDate ?: LocalDate.now()
+    }
 
     fun cancel() {
         //todo
