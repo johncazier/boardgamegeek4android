4eba071c4f472e184468aaa1c551c4939cb19e9a
johncazier <johncazier@gmail.com>
2025-05-24 09:35:32 -0600

    rename files so they are not confused with other new log play files

--- app/src/main/java/com/boardgamegeek/ui/ComposeLogPlayActivity.kt
+++ app/src/main/java/com/boardgamegeek/ui/ComposeLogPlayActivity.kt
@@ -6,25 +6,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import com.boardgamegeek.extensions.startActivity
-import com.boardgamegeek.ui.viewmodel.NewLogPlayViewModel
+import com.boardgamegeek.ui.viewmodel.ComposeLogPlayViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NewLogPlayActivity : ComponentActivity() {
+class ComposeLogPlayActivity : ComponentActivity() {
 
-    private val viewModel: NewLogPlayViewModel by viewModels()
+    private val viewModel: ComposeLogPlayViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewLogPlayScreen(viewModel)
+            ComposeLogPlayScreen(viewModel)
         }
     }
 
     companion object {
         fun start(context: Context, gameId: Int, gameName: String, imageUrl: String) {
-            context.startActivity<NewLogPlayActivity>(
-                "route" to NewLogPlayRoute(gameId, gameName, imageUrl)
+            context.startActivity<ComposeLogPlayActivity>(
+                "route" to ComposeLogPlayRoute(gameId, gameName, imageUrl)
             )
         }
     }
