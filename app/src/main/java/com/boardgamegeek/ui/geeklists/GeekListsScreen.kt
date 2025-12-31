954a3ce44f82dda6a06a00f75183ae98e512bcd5
johncazier <johncazier@gmail.com>
2025-12-31 11:28:12 -0700

    cleanup

--- app/src/main/java/com/boardgamegeek/ui/geeklists/GeekListsScreen.kt
+++ app/src/main/java/com/boardgamegeek/ui/geeklists/GeekListsScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
@@ -26,7 +29,9 @@ fun GeekListsScreen(
 ) {
     val lazyPagingItems = viewModel.geekLists.collectAsLazyPagingItems()
 
-    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(paddingValues)) {
         LazyColumn(
             modifier = Modifier.fillMaxSize()
         ) {
@@ -93,12 +98,17 @@ fun GeekListRow(geekList: GeekList) {
             .padding(16.dp)
     ) {
         Column(modifier = Modifier.weight(1f)) {
-            Text(text = geekList.title)
-            Text(text = geekList.username)
-        }
-        Column(horizontalAlignment = Alignment.End) {
-            Text(text = pluralStringResource(id = R.plurals.num_items, geekList.numberOfItems, geekList.numberOfItems))
-            Text(text = pluralStringResource(id = R.plurals.num_thumbs, geekList.numberOfThumbs, geekList.numberOfThumbs))
+            Text(text = geekList.title, style = MaterialTheme.typography.titleMedium)
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(painter = painterResource(R.drawable.ic_outline_account_circle_18), contentDescription = null, modifier = Modifier.padding(end = 4.dp))
+                Text(text = geekList.username, style = MaterialTheme.typography.labelSmall)
+                Text(text = "|", Modifier.padding(horizontal = 4.dp))
+                Icon(painter = painterResource(R.drawable.ic_outline_thumb_up_18), contentDescription = null, modifier = Modifier.padding(end = 4.dp))
+                Text(text = geekList.numberOfThumbs.toString(), style = MaterialTheme.typography.labelSmall)
+                Text(text = "|", Modifier.padding(horizontal = 4.dp))
+                Icon(painter = painterResource(R.drawable.ic_baseline_format_list_bulleted_18), contentDescription = null, modifier = Modifier.padding(end = 4.dp))
+                Text(text = geekList.numberOfItems.toString(), style = MaterialTheme.typography.labelSmall)
+            }
         }
     }
 }
